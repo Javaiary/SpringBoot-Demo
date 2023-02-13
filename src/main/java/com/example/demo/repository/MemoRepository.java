d5e918cd8220858ca9a7f39afa625a5f67cd7a00
Javaiary <narrchoii@gmail.com>
2023-02-13 14:00:50 +0900

    sort test

--- src/main/java/com/example/demo/repository/MemoRepository.java
+++ src/main/java/com/example/demo/repository/MemoRepository.java
@@ -1,7 +1,16 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Memo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findByIdBetweenOrderByIdDesc(Long from, Long to);
+
+    Page<Memo> findByIdBetween(Long from, Long to, Pageable pageable);
+
+    void deleteMemoryByIdLessThan(Long num);
 }
--- src/test/java/com/example/demo/MemoRepositoryTests.java
+++ src/test/java/com/example/demo/MemoRepositoryTests.java
@@ -8,8 +8,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 
 
+import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -21,7 +25,7 @@ public class MemoRepositoryTests {
 
     @Test   //create
     public void InsertDummies() {
-        IntStream.rangeClosed(1, 10).forEach(i -> {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
             Memo memo = Memo.builder()
                     .memoText("Sample....." + i)
                     .build();
@@ -36,7 +40,7 @@ public class MemoRepositoryTests {
     @Test // read
     public void selectDummies() {
 
-        Long id = 10L;
+        Long id = 100L;
 
         Optional<Memo> result = memoRepository.findById(id);
 
@@ -73,7 +77,7 @@ public class MemoRepositoryTests {
         System.out.println("Total Pages: " + result.getTotalPages());   //총 몇 페이지
         System.out.println("Total Count: " + result.getTotalElements());    // 전체 개수
         System.out.println("Page Number: " + result.getNumber());   // 현재 페이지 번호
-        System.out.println("Page Size: " + result.getNumber());     // 페이지당 데이터 개수
+        System.out.println("Page Size: " + result.getSize());     // 페이지당 데이터 개수
         System.out.println("has next page?: " + result.hasNext());  // 다음 페이지
         System.out.println("first page?: " + result.isFirst());     // 시작 페이지(0) 여부
 
@@ -83,5 +87,38 @@ public class MemoRepositoryTests {
         }
     }
 
+    @Test
+    public void testSort(){
+        Sort sort1 = Sort.by("id").descending();
+        Sort sort2 = Sort.by("memoText").ascending();
+        Sort sortAll = sort1.and(sort2);        // and를 이용한 연결
+        Pageable pageable = PageRequest.of(0,10, sortAll);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        result.get().forEach(memo -> {
+            System.out.println(memo);
+        });
+    }
+    @Test
+    public void testQueryMethods() {
+        List<Memo> list = memoRepository.findByIdBetweenOrderByIdDesc(70L, 80L);
+
+        for (Memo memo : list){
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testQueryMethodWithPagable() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("id").descending());
+        Page<Memo> result = memoRepository.findByIdBetween(10L, 50L, pageable);
+        result.get().forEach(memo -> System.out.println(memo));
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteQueryMethods() {
+        memoRepository.deleteMemoryByIdLessThan(10L);
+    }
 
 }
