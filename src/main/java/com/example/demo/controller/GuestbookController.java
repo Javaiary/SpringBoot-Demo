ec3f54bffa90d55b81dbc9478f664dc709ecf4f2
Javaiary <narrchoii@gmail.com>
2023-02-20 19:09:02 +0900

    Guestbook List

--- src/main/java/com/example/demo/controller/GuestbookController.java
+++ src/main/java/com/example/demo/controller/GuestbookController.java
@@ -1,19 +1,63 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.dto.GuestbookDTO;
+import com.example.demo.dto.PageRequestDTO;
+import com.example.demo.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor // 자동주입을 위한 Annotation
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
-    public String list(){
-        log.info("list.................");
-        return "/guestbook/list";
+    private final GuestbookService service; // final로 선언
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        log.info("list............." + pageRequestDTO);
+
+        model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto..." + dto);
+
+        // 새로 추가된 엔티티의 번호
+        Long gno = service.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/read")
+    public void read(long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model){
+        log.info("gno: " + gno);
+
+        GuestbookDTO dto = service.read(gno);
+
+        model.addAttribute("dto", dto);
     }
 }
