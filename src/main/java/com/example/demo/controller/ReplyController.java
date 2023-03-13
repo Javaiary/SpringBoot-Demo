1b25cf3850cf1a4432de05d35fff22a881d77245
Javaiary <narrchoii@gmail.com>
2023-03-13 17:57:49 +0900

    Reply register, modify, remove

--- src/main/java/com/example/demo/controller/ReplyController.java
+++ src/main/java/com/example/demo/controller/ReplyController.java
@@ -5,14 +5,12 @@ import com.example.demo.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpRange;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,29 @@ public class ReplyController {
         return new ResponseEntity<>(replyService.getList(bno), HttpStatus.OK);
     }
 
+    @PostMapping("")
+    public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO){
+        log.info(replyDTO);
+
+        Long rno = replyService.register(replyDTO);
+
+        return new ResponseEntity<>(rno, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{rno}")    // @Mapping: url 변경 없이 데이터 전송 처리가 가능
+    public ResponseEntity<String> remove(@PathVariable("rno") Long rno){
+        log.info("RNO: " + rno);
+
+        replyService.remove(rno);
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+        @PutMapping("/{rno}")
+        public ResponseEntity<String> modify(@RequestBody ReplyDTO replyDTO){
+            log.info(replyDTO);
+
+            replyService.modify(replyDTO);
+
+            return new ResponseEntity<>("success", HttpStatus.OK);
+        }
 }
 
