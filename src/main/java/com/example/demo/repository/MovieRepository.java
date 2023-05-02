c6804bab66fc906086d542b65ea242cec97468ce
Javaiary <narrchoii@gmail.com>
2023-05-02 12:59:07 +0900

    Movie List Page

--- src/main/java/com/example/demo/repository/MovieRepository.java
+++ src/main/java/com/example/demo/repository/MovieRepository.java
@@ -6,9 +6,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-    @Query("select m, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m "
-            + "left outer join Review r on r.movie = m group by m")
+//    @Query("select m, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m "
+//            + "left outer join Review r on r.movie = m group by m")
+//    @Query("select m, max(mi), avg(coalesce(r.grade,0)), count(distinct r) from Movie m " +
+//            "left outer join MovieImage mi on mi.movie = m " +
+//            "left outer join Review r on r.movie = m group by m")
+    @Query("Select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m " +
+        "left outer join MovieImage mi on mi.movie = m " +
+        "left outer join Review r on r.movie = m group by m, mi")
     Page<Object[]> getListPage(Pageable pageable);
+
 
 }
